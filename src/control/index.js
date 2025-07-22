e77dd2aae6bf2d044b9b1d7e52a4165ae14f7c21
ljcz-fujia <33539190+ljcz-fujia@users.noreply.github.com>
2025-07-22 10:32:34 +0800

    New feature for GamePad&Joystick control
    Ref: https://github.com/chvin/react-tetris/commit/1e31eed7aaa19446f9f5ebd60cd42640d1bb7c8f

--- src/control/index.js
+++ src/control/index.js
@@ -11,6 +11,70 @@ const keyboard = {
   82: 'r',
   80: 'p',
 };
+// add for Gamepad 20221101
+let ButtonActive = 0;
+const update = () => {
+  for (const gamepad of navigator.getGamepads()) {
+    //  if (!gamepad) continue;
+    for (const [index, button] of gamepad.buttons.entries()) {
+      // 1 left Button0
+      if (index === 0 && button.touched) {
+        const type = keyboard[37];// keycode
+        if (ButtonActive === 0) {
+          todo[type].down(store);
+          todo[type].up(store);
+          ButtonActive = 1;
+        }
+      } else if (index === 0 && !button.touched) {
+        if (ButtonActive === 1) {
+          ButtonActive = 0;
+        }
+      }
+      // 2 rotate Button3
+      if (index === 3 && button.touched) {
+        const type = keyboard[38];// keycode
+        if (ButtonActive === 0) {
+          todo[type].down(store);
+          todo[type].up(store);
+          ButtonActive = 2;
+        }
+      } else if (index === 3 && !button.touched) {
+        if (ButtonActive === 2) {
+          ButtonActive = 0;
+        }
+      }
+      // 3 right Button1
+      if (index === 1 && button.touched) {
+        const type = keyboard[39];// keycode
+        if (ButtonActive === 0) {
+          todo[type].down(store);
+          todo[type].up(store);
+          ButtonActive = 3;
+        }
+      } else if (index === 1 && !button.touched) {
+        if (ButtonActive === 3) {
+          ButtonActive = 0;
+        }
+      }
+      // 4 space Button2
+      if (index === 2 && button.touched) {
+        const type = keyboard[32];// keycode
+        if (ButtonActive === 0) {
+          todo[type].down(store);
+          todo[type].up(store);
+          ButtonActive = 4;
+        }
+      } else if (index === 2 && !button.touched) {
+        if (ButtonActive === 4) {
+          ButtonActive = 0;
+        }
+      }
+    }
+  }
+};
+
+setInterval(update, 100);
+// add for Gamepad 20221101
 
 let keydownActive;
 
@@ -41,4 +105,5 @@ const keyUp = (e) => {
 
 document.addEventListener('keydown', keyDown, true);
 document.addEventListener('keyup', keyUp, true);
-
+// add for Gamepad 20221031
+window.addEventListener('gamepadconnected', update, true);
